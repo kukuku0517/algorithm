e1b16b22aa0963aa28bcc01dda42b1cc54a367ec
KJH <kukuku0517@gmail.com>
2018-02-18 00:18:58 +0900

    180218_6549_unsolved

--- src/DS/_6549.java
+++ src/DS/_6549.java
@@ -8,37 +8,43 @@ public class _6549 {
         Scanner scan = new Scanner(System.in);
         while (scan.hasNext()) {
             int count = scan.nextInt();
+            if (count == 0) break;
             int[] list = new int[count];
             for (int i = 0; i < count; i++) {
                 list[i] = scan.nextInt();
             }
 
             int max = 0;
-            Stack<Integer> s = new Stack<>();
-            for (int i = 0; i < count; i++) {
-                    int c = 1;
-                    while (!s.isEmpty() && s.peek() > list[i]) {
-                        int temp = s.pop();
-//                        System.out.printf("%d * %d = %d\n",temp,c,temp*c);
-                        max = Math.max(c * temp, max);
-//                        System.out.printf("max : %d \n",max);
-                        c++;
-                    }
-//                    System.out.printf("push %dth %d\n",i,list[i]);
-                    s.push(list[i]);
+            Stack<Node> s = new Stack<>();
 
+            for (int i = 0; i < count; i++) {
+                while (!s.isEmpty() && s.peek().h > list[i]) {
+                    Node temp = s.pop();
+                    int width = i;
+                    if (!s.isEmpty()) width = i - temp.i - 1;
+                    System.out.printf("(%d,%d) * %d\n", temp.i - width + 1, temp.i, temp.h);
+                    max = Math.max(width * temp.h, max);
+                }
+                s.push(new Node(list[i], i));
             }
 
-            int c = 1;
             while (!s.isEmpty()) {
-                int temp = s.pop();
-                max = Math.max(c * temp, max);
-                c++;
+                Node temp = s.pop();
+                int width = count;
+                if (!s.isEmpty()) width = count - s.peek().i - 1;
+                max = Math.max(width * temp.h, max);
             }
             System.out.println(max);
-
-
         }
     }
 
+    static class Node {
+        int h;
+        int i;
+
+        Node(int h, int i) {
+            this.h = h;
+            this.i = i;
+        }
+    }
 }
