344e2327cb1847eadece7607e7181fb73881bdcb
KJH <kukuku0517@gmail.com>
2018-02-22 16:22:19 +0900

    180222_4195_unsolved

--- src/DS/_4195.java
+++ src/DS/_4195.java
@@ -1,6 +1,8 @@
 package DS;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
 
 public class _4195 {
@@ -8,53 +10,51 @@ public class _4195 {
         Scanner scan = new Scanner(System.in);
         int count = scan.nextInt();
 
+
         for (int i = 0; i < count; i++) {
-            HashMap<String, Node> list = new HashMap<>();
+            HashMap<String, Integer> counts = new HashMap<>();
+            HashMap<String, String> root = new HashMap<>();
+
             int input = scan.nextInt();
+            int max = -1;
             for (int j = 0; j < input; j++) {
                 String x = scan.next();
                 String y = scan.next();
-                if(!list.containsKey(x)){
-                    list.put(x,new Node(1,x));
+                if (!root.containsKey(x)) {
+                    root.put(x, x);
+                    counts.put(x, 1);
                 }
-                if(!list.containsKey(y)){
-                    list.put(y,new Node(1,y));
+                if (!root.containsKey(y)) {
+                    root.put(y, y);
+                    counts.put(y, 1);
                 }
-
-                union(list, x, y);
-                System.out.println(list.get(x).count);
+                int temp = union(counts, root, x, y);
+                max = Math.max(temp,max);
+                System.out.println(max);
             }
         }
     }
 
-    static void union(HashMap<String, Node> list, String x, String y) {
-        x = find(list, list.get(x),list.get(x).count);
-        y = find(list, list.get(y),list.get(y).count);
-        list.get(y).root = list.get(x);
-
+    static int union(HashMap<String, Integer> counts,
+                      HashMap<String, String> root, String x, String y) {
+        x = find(root, x);
+        y = find(root, y);
+        if (x != y) {
+            int temp = counts.get(x) + counts.get(y);
+            counts.put(x, temp);
+            root.put(y, x);
+            return temp;
+        }
+        return -1;
     }
 
-    static String find(HashMap<String, Node> list, Node x, int count) {
-        if (list.get(x.name).name == x.name) {
-            x.count += count;
-            return x.name;
+    static String find(HashMap<String, String> root, String x) {
+        if (root.get(x) == x) {
+            return x;
         } else {
-//            return root.put(x, find(root, rootCount, root.get(x)));
-            x.root = list.get(find(list, x.root, x.count));
-            return x.name;
+            String temp = find(root, root.get(x));
+            root.put(x, temp);
+            return temp;
         }
     }
-
-    static class Node {
-        Node root;
-        int count;
-        String name;
-
-        Node(int count, String name) {
-            this.count = count;
-            this.name = name;
-        }
-    }
-
-
 }
